c9f4a2b5afe480052aa9b532f09eaef6a567f7f1
ethanjohnson <john64862@stthomas.edu>
2024-07-23 15:23:56 -0500

    Added Keyboard and Mouse inputs

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -11,7 +11,8 @@ public class GameWindow extends JFrame{
         jframe.setSize(400, 400);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.add(gamePanel);
+        jframe.setLocationRelativeTo(null);
         jframe.setVisible(true);
     }
-    
+
 }
