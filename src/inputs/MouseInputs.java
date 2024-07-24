ec232d4ca11af21dcc5aebc8963f614b746d909c
ethanjohnson <john64862@stthomas.edu>
2024-07-23 22:56:23 -0500

    Adding Images

--- src/inputs/MouseInputs.java
+++ src/inputs/MouseInputs.java
@@ -20,7 +20,7 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        gamePanel.setRectPos(e.getX(), e.getY());
+        System.out.println("Mouse moved");
     }
 
     @Override
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,11 @@
 package main;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
@@ -8,36 +13,63 @@ import inputs.MouseInputs;
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
-
+    private float xDelta = 100, yDelta = 100;
+    private BufferedImage img;
+    
     public GamePanel() {
         
         mouseInputs = new MouseInputs(this);
+
+        importImg();
+
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
 
     }
 
+    //Image is not being found while grabbing
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+        System.out.println(is);
+
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+            
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280,800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
+    }
+
     public void changeXDelta(int value) {
         this.xDelta += value;
-        repaint();
+        
     }
 
     public void changeYDelta(int value) {
         this.yDelta += value;
-        repaint();
+        
     }
 
     public void setRectPos(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
+        
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(xDelta, yDelta, 200, 50);
+        //g.drawImage(null, x, y, null);
+
     }
+
 }
