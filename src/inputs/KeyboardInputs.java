42a6a50f3baacbd8da9afe5efb922c4ebcf5db14
ethanjohnson <john64862@stthomas.edu>
2024-07-24 00:16:20 -0500

    Sprites change with keyboard inputs

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -3,7 +3,9 @@ package inputs;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import main.Game;
 import main.GamePanel;
+import static utilz.Constants.Directions.*;
 
 public class KeyboardInputs implements KeyListener{
 
@@ -19,23 +21,30 @@ public class KeyboardInputs implements KeyListener{
 
     @Override
     public void keyReleased(KeyEvent e) {
-        System.out.println("Key released");
+        switch(e.getKeyCode()) {
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_A:
+            case KeyEvent.VK_S:
+            case KeyEvent.VK_D:
+                gamePanel.setMoving(false);
+                break;
+        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         switch(e.getKeyCode()) {
             case KeyEvent.VK_W:
-                gamePanel.changeYDelta(-5);
+                gamePanel.setDirection(UP);
                 break;
             case KeyEvent.VK_A:
-                gamePanel.changeXDelta(-5);
+                gamePanel.setDirection(LEFT);
                 break;
             case KeyEvent.VK_S:
-                gamePanel.changeYDelta(5);
+                gamePanel.setDirection(DOWN);
                 break;
             case KeyEvent.VK_D:
-                gamePanel.changeXDelta(5);
+                gamePanel.setDirection(RIGHT);
                 break;
         }
     }
