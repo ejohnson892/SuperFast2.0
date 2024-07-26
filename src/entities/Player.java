71d2dd180a4a65c30993515504f9db1da41ee48e
ethanjohnson <john64862@stthomas.edu>
2024-07-25 22:49:58 -0500

    Added entity hitboxes

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,35 +1,28 @@
 package entities;
 
-import static utilz.Constants.Directions.DOWN;
-import static utilz.Constants.Directions.LEFT;
-import static utilz.Constants.Directions.RIGHT;
-import static utilz.Constants.Directions.UP;
-import static utilz.Constants.PlayerConstants.ATTACK_1;
-import static utilz.Constants.PlayerConstants.GetSpriteAmount;
-import static utilz.Constants.PlayerConstants.IDLE;
-import static utilz.Constants.PlayerConstants.RUNNING;
-
+import static utilz.Constants.PlayerConstants.*;
+import static utilz.HelpMethods.CanMoveHere;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
 
-import javax.imageio.ImageIO;
-
+import main.Game;
 import utilz.LoadSave;
 
 public class Player extends Entity {
     private BufferedImage[][] animations;
-    private int aniTick, aniIndex, aniSpeed = 15;
+    private int aniTick, aniIndex, aniSpeed = 20;
     private int playerAction = IDLE;
-    private int playerDir = -1;
     private boolean moving = false, attacking = false;
     private boolean left, right, up, down;
     private float playerSpeed = 2.0f;
+    private int[][] lvlData;
+    private float xDrawOffset = 21 * Game.SCALE;
+    private float yDrawOffset = 4 * Game.SCALE;
 
     public Player(float x, float y, int width, int height) {
         super(x, y, width, height);
         loadAnimations();
+        initHitbox(x, y, 20 * Game.SCALE, 28 * Game.SCALE);
     }
 
     public void update() {
@@ -39,7 +32,8 @@ public class Player extends Entity {
     }
 
     public void render(Graphics g) {
-        g.drawImage(animations[playerAction][aniIndex], (int) x, (int) y, width, height, null);
+        g.drawImage(animations[playerAction][aniIndex], (int)(hitbox.x - xDrawOffset), (int)(hitbox.y - yDrawOffset), width, height, null);
+        drawHitbox(g);
     }
 
 
@@ -83,20 +77,31 @@ public class Player extends Entity {
     private void updatePos() {
 
         moving = false;
-
-        if(left && !right) {
-            x -= playerSpeed;
-            moving = true;
-        } else if (right && !left) {
-            x += playerSpeed;
-            moving = true;
+        if(!left && !right && !up && !down) {
+            return;
         }
 
+        float xSpeed = 0, ySpeed = 0;
+
+        if(left && !right) {
+            xSpeed = -playerSpeed;
+        } else if (right && !left) {
+            xSpeed = playerSpeed;
+        }
         if(up && !down) {
-            y -= playerSpeed;
-            moving = true;
+            ySpeed = -playerSpeed;
         } else if (down && !up) {
-            y += playerSpeed;
+            ySpeed = playerSpeed;
+        }
+        // if(CanMoveHere(x + xSpeed, y + ySpeed, width, height, lvlData)) {
+        //     this.x += xSpeed;
+        //     this.y += ySpeed;
+        //     moving = true;
+        // }
+
+        if(CanMoveHere(hitbox.x + xSpeed, hitbox.y + ySpeed, hitbox.width, hitbox.height, lvlData)) {
+            hitbox.x += xSpeed;
+            hitbox.y += ySpeed;
             moving = true;
         }
     }
@@ -111,6 +116,10 @@ public class Player extends Entity {
                 animations[j][i] = img.getSubimage(i*64, j*40, 64, 40);
             }
         }
+    }
+
+    public void loadLvlData(int[][] lvlData) {
+        this.lvlData = lvlData;
     }
 
     public void resetDirBooleans() {
--- src/levels/LevelManager.java
+++ src/levels/LevelManager.java
@@ -46,4 +46,8 @@ public class LevelManager {
          
     }
 
+    public Level getCurrentLevel() {
+        return levelOne;
+    }
+
 }
